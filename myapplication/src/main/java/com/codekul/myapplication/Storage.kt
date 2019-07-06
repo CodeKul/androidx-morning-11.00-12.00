5476d14d384035f8dabeb10dff200dfa6d4dcae5
aniruddha <aniruddha.kudalkar@gmail.com>
2019-07-06 15:33:23 +0530

    storage

---
+++ myapplication/src/main/java/com/codekul/myapplication/Storage.kt
@@ -0,0 +1,12 @@
+package com.codekul.myapplication
+
+import android.app.Activity
+import android.os.Bundle
+
+class Storage : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_storage)
+    }
+}

e5dfa3b84365a418ceaba1a74428084be6e94b1a
aniruddha <aniruddha.kudalkar@gmail.com>
2019-07-10 14:39:52 +0530

    md : basics to advance material design

---
+++ materialdesignbasic/src/main/java/com/codekul/materialdesignbasic/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.codekul.materialdesignbasic
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                txt.text = "Settings"
+                return true
+            }
+            R.id.action_cut -> {
+                txt.text = "Cut"
+                return true
+            }
+            R.id.action_copy -> {
+                txt.text = "Copy"
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
